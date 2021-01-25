840d278fcb87c6613bd097ebd5f3e36695f4e375
Vittorio <vittoriogiammarino@gmail.com>
2021-01-25 14:52:14 -0500

    nested parallelization

--- OnlineBWforHIL_GridWorld/Neural_Network/Train_Online_Parallel.py
+++ OnlineBWforHIL_GridWorld/Neural_Network/Train_Online_Parallel.py
@@ -15,7 +15,8 @@ import time
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tensorflow import keras
-import multiprocessing as mp
+import multiprocessing
+import multiprocessing.pool
 
 # %% Expert Data
 
@@ -34,9 +35,59 @@ pi_hi_expert, pi_lo_expert, pi_b_expert = expert.HierarchicalPolicy()
 ExpertSim = expert.Simulation_tabular(pi_hi_expert, pi_lo_expert, pi_b_expert)
 
 max_epoch = 100 #max iterations in the simulation per trajectory
-nTraj = np.array([1]) #number of trajectories generated
+nTraj = nTraj = np.array([1, 2]) #np.array([1, 2, 5, 10, 20, 30, 50])#number of trajectories generated
 
 #%%
+
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
+
+def DifferentTrainingSet(i, nTraj, TrainingSet_tot, Labels_tot, TimeBatch, seed):
+    max_epoch = 100
+    TrainingSet = np.concatenate((TrainingSet_tot[0:max_epoch*nTraj[i],:],TrainingSet_tot[0:max_epoch*nTraj[i],:],TrainingSet_tot[0:max_epoch*nTraj[i],:],TrainingSet_tot[0:max_epoch*nTraj[i],:]),axis=0)
+    Labels = np.concatenate((Labels_tot[0:max_epoch*nTraj[i]],Labels_tot[0:max_epoch*nTraj[i]],Labels_tot[0:max_epoch*nTraj[i]],Labels_tot[0:max_epoch*nTraj[i]]),axis=0)
+    option_space = 2
+        
+    #Stopping Time
+    StoppingTime = TimeBatch[i]
+        
+    # Online BW for HIL with tabular parameterization: Training
+    M_step_epoch = 30
+    optimizer = keras.optimizers.Adamax(learning_rate=1e-2)
+    Agent_OnlineHIL = OnlineBW_HIL.OnlineHIL(TrainingSet, Labels, option_space, M_step_epoch, optimizer)
+    T_min = len(TrainingSet)/(4) - 20
+    start_online_time = time.time()
+    pi_hi_online, pi_lo_online, pi_b_online, likelihood_online, time_per_iteration = Agent_OnlineHIL.Online_Baum_Welch_together(T_min, StoppingTime)
+    end_online_time = time.time()
+    Online_time = end_online_time-start_online_time
+    # Time_array_online = np.append(Time_array_online, Online_time)  
+    # Likelihood_online_list.append(likelihood_online)
+    # time_likelihood_online_list.append(time_per_iteration)
+    
+    # Batch Agent Evaluation
+    nTraj_eval = 100
+    # Online Agent Evaluation
+    OnlineSim = expert.Simulation_NN(pi_hi_online, pi_lo_online, pi_b_online)
+    [trajOnline, controlOnline, OptionsOnline, 
+    TerminationOnline, psiOnline, rewardOnline] = OnlineSim.HierarchicalStochasticSampleTrajMDP(max_epoch, nTraj_eval, seed)
+    AverageRewardOnline = np.sum(rewardOnline)/nTraj_eval  
+    STDOnline = np.std(rewardOnline)
+    # RewardOnline_array = np.append(RewardOnline_array, AverageRewardOnline)
+    # STDOnline_array = np.append(STDOnline_array, STDOnline)
+    
+    return Online_time, likelihood_online, time_per_iteration, AverageRewardOnline, STDOnline
+
 
 def train(seed, TrainingSet_Array, Labels_Array, List_TimeBatch, max_epoch, nTraj):
     #seed
@@ -52,72 +103,43 @@ def train(seed, TrainingSet_Array, Labels_Array, List_TimeBatch, max_epoch, nTra
     STDOnline_array = np.empty((0))
     Likelihood_online_list = []
     time_likelihood_online_list =[]
-    
-    expert = World.TwoRewards.Expert()
 
-    for seed in range(1): #range(TrainingSet_Array.shape[0]):
-        TrainingSet_tot = TrainingSet_Array[seed, :, :]
-        Labels_tot = Labels_Array[seed, :, :]
-        TimeBatch = List_TimeBatch[seed]
-        for i in range(len(nTraj)):
-            TrainingSet = np.concatenate((TrainingSet_tot[0:max_epoch*nTraj[i],:],TrainingSet_tot[0:max_epoch*nTraj[i],:],TrainingSet_tot[0:max_epoch*nTraj[i],:],TrainingSet_tot[0:max_epoch*nTraj[i],:]),axis=0)
-            Labels = np.concatenate((Labels_tot[0:max_epoch*nTraj[i]],Labels_tot[0:max_epoch*nTraj[i]],Labels_tot[0:max_epoch*nTraj[i]],Labels_tot[0:max_epoch*nTraj[i]]),axis=0)
-            option_space = 2
+    TrainingSet_tot = TrainingSet_Array[seed, :, :]
+    Labels_tot = Labels_Array[seed, :, :]
+    TimeBatch = List_TimeBatch[0]
         
-            #Stopping Time
-            StoppingTime = TimeBatch[i]
-        
-            # Online BW for HIL with tabular parameterization: Training
-            M_step_epoch = 30
-            optimizer = keras.optimizers.Adamax(learning_rate=1e-2)
-            Agent_OnlineHIL = OnlineBW_HIL.OnlineHIL(TrainingSet, Labels, option_space, M_step_epoch, optimizer)
-            T_min = len(TrainingSet)/(4) - 20
-            start_online_time = time.time()
-            pi_hi_online, pi_lo_online, pi_b_online, likelihood_online, time_per_iteration = Agent_OnlineHIL.Online_Baum_Welch_together(T_min, StoppingTime)
-            end_online_time = time.time()
-            Online_time = end_online_time-start_online_time
-            Time_array_online = np.append(Time_array_online, Online_time)  
-            Likelihood_online_list.append(likelihood_online)
-            time_likelihood_online_list.append(time_per_iteration)
+    pool = multiprocessing.Pool(processes=len(nTraj))
+    args = [(i, nTraj, TrainingSet_tot, Labels_tot, TimeBatch, seed) for i in range(len(nTraj))]
+    givenSeed_training_results = pool.starmap(DifferentTrainingSet, args) 
     
-            # Batch Agent Evaluation
-            nTraj_eval = 100
-            # Online Agent Evaluation
-            OnlineSim = expert.Simulation_NN(pi_hi_online, pi_lo_online, pi_b_online)
-            [trajOnline, controlOnline, OptionsOnline, 
-             TerminationOnline, psiOnline, rewardOnline] = OnlineSim.HierarchicalStochasticSampleTrajMDP(max_epoch,nTraj_eval, seed)
-            AverageRewardOnline = np.sum(rewardOnline)/nTraj_eval  
-            STDOnline = np.std(rewardOnline)
-            RewardOnline_array = np.append(RewardOnline_array, AverageRewardOnline)
-            STDOnline_array = np.append(STDOnline_array, STDOnline)
+    pool.close()
+    pool.join()
+    
+    for i in range(len(nTraj)):
+        Time_array_online = np.append(Time_array_online, givenSeed_training_results[i][0]) 
+        Likelihood_online_list.append(givenSeed_training_results[i][1])
+        time_likelihood_online_list.append(givenSeed_training_results[i][2])
+        RewardOnline_array = np.append(RewardOnline_array, givenSeed_training_results[i][3])
+        STDOnline_array = np.append(STDOnline_array, givenSeed_training_results[i][4])
         
-        List_TimeOnline.append(Time_array_online)
-        List_RewardOnline.append(RewardOnline_array)
-        List_STDOnline.append(STDOnline_array)
-        List_LikelihoodOnline.append(Likelihood_online_list)
-        List_TimeLikelihoodOnline.append(time_likelihood_online_list)
+    List_TimeOnline.append(Time_array_online)
+    List_RewardOnline.append(RewardOnline_array)
+    List_STDOnline.append(STDOnline_array)
+    List_LikelihoodOnline.append(Likelihood_online_list)
+    List_TimeLikelihoodOnline.append(time_likelihood_online_list)
         
     return List_TimeOnline, List_RewardOnline, List_STDOnline, List_LikelihoodOnline, List_TimeLikelihoodOnline
 
 
-pool = mp.Pool(processes=10)
+pool = MyPool(10)
 args = [(seed, TrainingSet_Array, Labels_Array, List_TimeBatch, max_epoch, nTraj) for seed in range(10)]
 results_online = pool.starmap(train, args) 
-
+pool.close()
+pool.join()
 
 # %%
 
-# with open('Comparison/Online/List_TimeOnline.npy', 'wb') as f:
-#     np.save(f, List_TimeOnline)
+with open('Comparison/Online/results_online.npy', 'wb') as f:
+    np.save(f, results_online)
     
-# with open('Comparison/Online/List_RewardOnline.npy', 'wb') as f:
-#     np.save(f, List_RewardOnline)
-    
-# with open('Comparison/Online/List_STDOnline.npy', 'wb') as f:
-#     np.save(f, List_STDOnline)
-    
-# with open('Comparison/Online/List_LikelihoodOnline.npy', 'wb') as f:
-#     np.save(f, List_LikelihoodOnline)
-    
-# with open('Comparison/Online/List_TimeLikelihoodOnline.npy', 'wb') as f:
-#     np.save(f, List_TimeLikelihoodOnline)
+
